2b9f040136f5562e179145b3e122933fd16b7a06
OOHYEE <azss0470@gmail.com>
2021-09-08 11:13:31 +0900

    Data Validation

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
 const db = require("../database");
+
+const { check, validationResult } = require("express-validator");
+
 const router = Router();
 
 router.use((req, res, next) => {
@@ -16,18 +19,35 @@ router.get("/posts", (req, res) => {
   res.json({ route: "Posts" });
 });
 
-router.post("/", (req, res) => {
-  const { username, password } = req.body;
-  if (username && password) {
-    try {
-      db.promise().query(
-        `INSERT INTO USERS VALUES('${username}', '${password}')`
-      );
-      res.status(201).send({ msg: "Created User" });
-    } catch (err) {
-      console.log(err);
+router.post(
+  "/",
+  [
+    check("username")
+      .notEmpty()
+      .withMessage("Username cannot be empty")
+      .isLength({ min: 5 })
+      .withMessage("Username must be at least 5 characters"),
+    check("password").notEmpty().withMessage("Password cannot be empty"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+    if (username && password) {
+      try {
+        db.promise().query(
+          `INSERT INTO USERS VALUES('${username}', '${password}')`
+        );
+        res.status(201).send({ msg: "Created User" });
+      } catch (err) {
+        console.log(err);
+      }
     }
   }
-});
+);
 
 module.exports = router;
